0ec7cda86125dff9c8a44ef42820049b0332982c
MatDagommer <matthieu.dagommer@gmail.com>
2024-02-15 22:10:17 -0500

    switched to decreasing kl annealing

--- chemvae_torch/utils_torch.py
+++ chemvae_torch/utils_torch.py
@@ -157,5 +157,5 @@ def sigmoid_schedule(time_step, slope=1.0, start=None, weight_orig=None):
     """
     # Inverted float(time_step) and start wrt the original function
     # The function should be decreasing with the time_step for weight annealing
-    # return weight_orig * float(1 / (1.0 + np.exp(slope * (float(time_step) - start))))
-    return weight_orig * float(1 / (1.0 + np.exp(slope * (start - float(time_step)))))
+    return weight_orig * float(1 / (1.0 + np.exp(slope * (float(time_step) - start))))
+    # return weight_orig * float(1 / (1.0 + np.exp(slope * (start - float(time_step)))))
