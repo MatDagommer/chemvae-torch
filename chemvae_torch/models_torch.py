fad041ce6f5b8bd80c18d5ddcdf29cdafe0655a1
MatDagommer <matthieu.dagommer@gmail.com>
2024-02-21 12:13:44 -0500

    testing without custom batch norm

--- chemvae_torch/models_torch.py
+++ chemvae_torch/models_torch.py
@@ -209,7 +209,8 @@ class DecoderModel(nn.Module):
                     "gru_{}".format(i), nn.GRU(params["recurrent_dim"], params["recurrent_dim"], batch_first=True)
                 )
 
-        self.x_out = CustomGRU(params["recurrent_dim"], params["NCHARS"], 1)
+        # self.x_out = CustomGRU(params["recurrent_dim"], params["NCHARS"], 1)
+        self.x_out = nn.GRU(params["recurrent_dim"], params["NCHARS"], 1)
 
     def forward(self, z_in, targets=None):
         """
@@ -233,7 +234,8 @@ class DecoderModel(nn.Module):
 
         if self.training:
             # teacher forcing with the targets
-            x_out, _ = self.x_out.forward(x_dec, targets=targets)
+            # x_out, _ = self.x_out.forward(x_dec, targets=targets)
+            x_out, _ = self.x_out.forward(x_dec)
         else:
             x_out, _ = self.x_out.forward(x_dec)
 
